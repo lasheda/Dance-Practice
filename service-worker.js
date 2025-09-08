dc61d135841e91ab341230d2dfee9d4435179a72
lasheda <144438920+lasheda@users.noreply.github.com>
2025-09-08 10:46:23 +0200

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,2 +0,0 @@
-self.addEventListener('install', e => { e.waitUntil(caches.open('dmc-v10').then(cache => cache.addAll(['/','/index.html','/style.css','/script.js','/manifest.json']))); });
-self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(resp => resp || fetch(e.request))); });
